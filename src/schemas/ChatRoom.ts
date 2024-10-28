338ee4d986764d4541aa4ef9b14b8b315260db82
RB-Matheus <typematt4101@gmail.com>
2024-10-28 16:31:37 -0300

    Schema modeling for ChatRoom

--- src/schemas/ChatRoom.ts
+++ src/schemas/ChatRoom.ts
@@ -3,8 +3,18 @@ import { User } from './User';
 import { v4 as uuid } from 'uuid';
 
 type ChatRoom = Document & {
-  idUsers: User[];
   idChatRoom: string;
+  idUsers: User[];
+  idAdmins: User[];     // manipular quando um nó adm surgir ou cair
+  idBanned: User[];
+  idKickedOut: User[];
+  name: string;
+  isPrivate: boolean;   // conversa particular x grupo
+  description?: string;
+  createdAt: Date;
+  messagesCount: number;
+  userLimit: number;
+  // lastMessageAt?: Date; depois pensar na modelagem de nó ocioso
 }
 
 const ChatRoomSchema = new Schema({
@@ -14,9 +24,64 @@ const ChatRoomSchema = new Schema({
       ref: 'Users',
     },
   ],
+
+  idAdmins: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Users',
+    }
+  ],
+
+  idBanned: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Users',
+    }
+  ],
+
+  idKickedOut: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Users',
+    }
+  ],
+
   idChatRoom: {
     type: String,
     default: uuid,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  isPrivate: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+
+  description: {
+    type: String,
+    default: '',
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  
+  messagesCount: {
+    type: Number,
+    default: 0,
+  },
+
+  userLimit: {
+    type: Number,
+    required: true,
+    default: 2,
+    max: 50,
   },
 });
 
