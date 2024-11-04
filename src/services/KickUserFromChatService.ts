283dc9a132c7e4b51020da5cc00d2cc80d73224f
RB-Matheus <typematt4101@gmail.com>
2024-11-04 14:53:12 -0300

    [new feature] admin leaves the group

--- src/services/KickUserFromChatService.ts
+++ src/services/KickUserFromChatService.ts
@@ -1,5 +1,6 @@
 import { injectable } from "tsyringe";
 import { ChatRoom } from "../schemas/ChatRoom";
+import { User } from "../schemas/User";
 
 @injectable()
 export class KickUserFromChatService {
@@ -14,7 +15,8 @@ export class KickUserFromChatService {
   }) {
     const room = await ChatRoom.findOne({ idChatRoom: roomId })
       .populate('idAdmin')
-      .populate('idUsers');
+      .populate('idUsers')
+      .sort({ idUsersJoinedAt: 1 }); // Sorting users by join date;
 
     if (!room)
       throw new Error('Chat room not found');
@@ -23,12 +25,36 @@ export class KickUserFromChatService {
     if (String(room.idAdmin._id) !== String(adminId)) 
       throw new Error('Only admin can kick users');
 
-    // Removing user from room
+
+    // Check if the user to be kicked is the adm
+    if (String(room.idAdmin._id) === String(userId)) {
+      // Find the oldest user in the room (not including the adm)
+      const newAdmin = room.idUsers.find(
+        (user) => String(user._id) !== String(userId)
+      );
+
+      if (newAdmin) {
+        // Update the new adm in the room
+        await ChatRoom.findOneAndUpdate(
+          { idChatRoom: roomId },
+          {
+            idAdmin: newAdmin._id
+          },
+          { new: true }
+        );
+      } else {
+        // Adm is the only in the chat room, cannot kick
+        throw new Error('Cannot kick the only user in the room');
+      }
+    }
+
+    // Removing user from the room
     await ChatRoom.findOneAndUpdate(
       { idChatRoom: roomId },
       {
         $pull: { idUsers: userId }
-      }
+      },
+      { new: true }
     );
 
     const updatedRoom = await ChatRoom.findOne({ idChatRoom: roomId })
